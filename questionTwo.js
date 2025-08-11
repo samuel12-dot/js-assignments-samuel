b684dae6ec1b9c8a3ce3734d765b3764c80b6f4c
samuel12-dot <samuelogunduyile76@gmail.com>
2025-08-12 00:48:54 +0100

    feat: final changes

--- questionTwo.js
+++ questionTwo.js
@@ -7,17 +7,14 @@ let totalCost = priceTicket * numTickets
 
 let regexName = /^[A-Za-z\s]+$/
 let regexResult = regexName.test(myName)
-console.log(regexResult)
 
 
 let isStudent = age <= 25
-console.log(isStudent)
 
 let eligibleDiscount = isStudent ? "Yes" : "No"
 
 
 let discount = isStudent ? (totalCost * 0.9) : totalCost
-console.log(discount)
 
 let output = `Hello ${myName},\n\nYou are eligible for a student discount: ${eligibleDiscount}\n\nTotal tickets ${numTickets}\n\nTotal price: ${discount}`
 
