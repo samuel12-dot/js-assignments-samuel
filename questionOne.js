4eb2b582d2e3b1151e9e1e2e8e2ac39399d0ca33
samuel12-dot <samuelogunduyile76@gmail.com>
2025-08-13 10:24:44 +0100

    feat: modify

--- questionOne.js
+++ questionOne.js
@@ -7,7 +7,7 @@ let phoneNumber = "08090345678"
 let regexPhone = /^0\d{10}$/
 let phoneResult = regexPhone.test(phoneNumber)
 
-let universityName = "@university.edu"
+let universityName = "@nileuniversity.edu"
 
 let emailGeneration = result && phoneResult ? fullName.toLowerCase().split(' ').join('') + "_" + phoneNumber.slice(7) + universityName : "invalid!"
 
